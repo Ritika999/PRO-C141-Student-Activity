71ad869cc210c2655fe75c0aaa89e90cc42c1977
Ritika999 <70030520+Ritika999@users.noreply.github.com>
2023-10-04 14:25:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ movies_data = pd.read_csv('final.csv')
 
 app = Flask(__name__)
 
+# extracting important information from dataframe
 all_movies = movies_data[["original_title","poster_link","release_date","runtime","weighted_rating"]]
 
 liked_movies = []
@@ -12,7 +13,7 @@ not_liked_movies = []
 did_not_watch = []
 
 
-# extracting important information from dataframe
+
 
 
 # variables to store data
